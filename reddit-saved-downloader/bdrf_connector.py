e691d47c47270438bdd4cd762d7dbf03631ea4bf
Fabian Zürker Aguilar <fzuerker@gmail.com>
2022-04-09 14:03:17 +0200

    Add login screen and list of posts

--- reddit-saved-downloader/bdrf_connector.py
+++ reddit-saved-downloader/bdrf_connector.py
@@ -2,7 +2,7 @@ import io
 import re
 import threading
 import time
-
+from praw.models import Submission
 from bdfr.downloader import RedditDownloader
 from bdfr.configuration import Configuration
 import logging.handlers
@@ -14,30 +14,38 @@ logger = logging.getLogger()
 
 class BDRFConnector(object):
     def __init__(self):
+        self.downloader = None
+        self.oauth2_url = None
         self.config = Configuration()
         self.config.authenticate = True
         self.config.file_scheme = "{POSTID}"
         self.config.folder_scheme = "."
         self.config.user = ["me"]
         self.config.saved = True
+
         self.logger = logging.getLogger("bdfr.oauth2")
         self.log_stream = io.StringIO()
         ch = logging.StreamHandler(self.log_stream)
         self.logger.addHandler(ch)
         self.thread = threading.Thread(target=self.init_downloader)
         self.thread.start()
+
         time.sleep(1)
+
         r = re.compile(
-            "https:\/\/www\.reddit\.com\/api\/v1\/authorize\?client_id=.*&duration=permanent&redirect_uri=.*&response_type=code&scope=.*&state=.*"
+            r"https://www\.reddit\.com/api/v1/authorize\?client_id=.*&duration=permanent&redirect_uri"
+            r"=.*&response_type=code&scope=.*&state=.* "
         )
-        self.auth_url = re.findall(r, self.log_stream.getvalue())[0]
+        matches = re.findall(r, self.log_stream.getvalue())
+        if matches:
+            self.oauth2_url = matches[0]
 
     def init_downloader(self):
         self.downloader = RedditDownloader(self.config)
 
     def get_saved_posts(self):
         self.thread.join()
-        return self.downloader.reddit_lists
+        return [e for e in self.downloader.reddit_lists[0] if isinstance(e, Submission)]
 
     def download_post(self, submission):
         self.thread.join()
--- reddit-saved-downloader/main.py
+++ reddit-saved-downloader/main.py
@@ -1,5 +1,51 @@
+from PySimpleGUI import WIN_CLOSED
+import itertools
 from bdrf_connector import BDRFConnector
+import PySimpleGUI as sg
+import sys
+import webbrowser
 
-bdfr_connector = BDRFConnector()
+bdrf_connector = BDRFConnector()
 
-print(bdfr_connector.auth_url)
+if bdrf_connector.oauth2_url:
+    login_layout = [
+        [sg.Text("Login through this link")],
+        [sg.Text(bdrf_connector.oauth2_url, enable_events=True)],
+        [sg.Button("OK", key="OK")],
+    ]
+    login_window = sg.Window("Login", layout=login_layout)
+    while True:
+        event, values = login_window.read()
+        if event == bdrf_connector.oauth2_url:
+            webbrowser.open(bdrf_connector.oauth2_url)
+        if event == "OK":
+            login_window.close()
+            break
+        elif event is None:
+            sys.exit(0)
+
+saved_posts = bdrf_connector.get_saved_posts()
+print(saved_posts, len(saved_posts))
+
+layout = [
+    [
+        sg.Listbox(
+            values=[f"{e.id} - {e.title}" for e in saved_posts],
+            size=(60, 20),
+            key="listbox",
+        )
+    ]
+]
+
+window = sg.Window(
+    "Keyboard Test", layout, return_keyboard_events=True, use_default_focus=False
+)
+
+# ---===--- Loop taking in user input --- #
+while True:
+    event, values = window.read()
+    if event == WIN_CLOSED:
+        break
+
+
+window.close()
